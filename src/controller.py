d47181057ef11506669a94863c974d6fae909963
zhoyh <djangoyhz@gmail.com>
2018-03-29 18:39:41 +0800

    修改项目结构

--- src/controller.py
+++ src/controller.py
@@ -4,23 +4,23 @@ from data import pref_by_movie
 
 def Pearson(pref, movie1, movie2):
     # 找出对两部电影都评论的人
-    people_list = [person for person in pref[movie1].keys()
+    common_people = [person for person in pref[movie1].keys()
                    if person in pref[movie2].keys()]
-    n = len(people_list)
+    n = len(common_people)
     if n == 0:
         return 0
     # 计算评价和
-    sum1 = sum([pref[movie1][person] for person in people_list])
-    sum2 = sum([pref[movie2][person] for person in people_list])
+    sumX = sum([pref[movie1][person] for person in common_people])
+    sumY = sum([pref[movie2][person] for person in common_people])
     # 计算评价平方和
-    sumSq1 = sum([pref[movie1][person] ** 2 for person in people_list])
-    sumSq2 = sum([pref[movie2][person] ** 2 for person in people_list])
+    sumSq1 = sum([pref[movie1][person] ** 2 for person in common_people])
+    sumSq2 = sum([pref[movie2][person] ** 2 for person in common_people])
     # 计算评价成绩和
     psum = sum([pref[movie1][person] * pref[movie2][person]
-                for person in people_list])
+                for person in common_people])
     # 皮尔逊相关系数计算
-    num = psum - sum1 * sum2 / n
-    den = math.sqrt((sumSq1 - (sum1 ** 2) / n) * (sumSq2 - (sum2 ** 2) / n))
+    num = psum - sumX * sumY / n
+    den = math.sqrt((sumSq1 - (sumX ** 2) / n) * (sumSq2 - (sumY ** 2) / n))
 
     if den == 0:
         return 0
